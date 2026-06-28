5d9023fc733a31a15e5085c0bc072fef1cebad6d
Caio Henrique <caio.hsilva0209@gmail.com>
2026-06-28 18:10:12 -0300

    feat: Finalização do projeto primeflix

--- projeto-primeflix/src/pages/Filme/index.js
+++ projeto-primeflix/src/pages/Filme/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from 'react-router-dom';
 import './filme-info.css';
 import api from '../../services/api';
+import { toast } from 'react-toastify';
 
 function Filme() {
     const { id } = useParams();
@@ -41,16 +42,16 @@ function Filme() {
 
         let filmesSalvos = JSON.parse(minhaLista) || [];
 
-        const hasFilme = filmesSalvos.some( (filmesSalvos) => filmesSalvos.id === filme.id )
+        const hasFilme = filmesSalvos.some( (filmesSalvo) => filmesSalvo.id === filme.id )
 
         if(hasFilme){
-            alert("Filme ja está salvo na lista");
+            toast.warn("Esse filme ja está na lista")
             return;
         }
 
         filmesSalvos.push(filme);
         localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
-        alert("Filme salvo com sucesso");
+        toast.success("Filme salvo com sucesso");
 
     }
 
@@ -72,7 +73,7 @@ function Filme() {
             <strong>Avaliação: {filme.vote_average} / 10 </strong>
 
             <div className="area-buttons">
-                <button>Salvar</button>
+                <button onClick={salvarFilme}>Salvar</button>
                 <button>
                     <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer` }  >
                         Trailer
